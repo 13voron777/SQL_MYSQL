5900c026bc081b27e5a457fb4c4d8e2c60f52633
Oleg <olvor196@gmail.com>
2021-11-29 00:13:12 +0100

    HW1 cor

--- Lesson1/HW1/Task4&5.sql
+++ Lesson1/HW1/Task4&5.sql
@@ -1,7 +1,7 @@
 USE MyDB;
 
-# HW 1 Task 3
+# HW 1 Task 4
 SELECT id FROM emp_salaries WHERE salary > 10000;
 
-# HW 1 Task 4
+# HW 1 Task 5
 UPDATE emp_personal SET marital_status = "Married" WHERE id = 1;
