e013d876b07ec82455ed5758aea0abf5807d801f
Oleg <olvor196@gmail.com>
2021-12-09 20:01:46 +0100

    LAB6 1-4,7

--- Lesson6/LAB6/LAB6.sql
+++ Lesson6/LAB6/LAB6.sql
@@ -16,19 +16,24 @@ SELECT * FROM SubLab63;
 DROP INDEX FirstName ON 2f_author;
 SELECT * FROM 2f_author;
 CREATE INDEX FirstName ON 2f_author(FirstName);
+SELECT * FROM 2f_author;
 
 # 5.Написать представление, которое будет отображать информацию о книгах, которые имели тираж более 10 экземпляров.
 CREATE VIEW SubLab65 AS
 SELECT * FROM 2f_books_sales
 WHERE Price > 55;
-SELECT * FROM SubLab63;
+SELECT * FROM SubLab65;
 
-# 6. Написать представление, которое содержит информацию о суммах, на которые были проданы книги каждым магазином за последний месяц.
-
+# 6. Написать представление, которое содержит информацию о суммах, на которые были проданы книги каждым магазином.
+CREATE VIEW SubLab66 AS
+SELECT * FROM 2f_books_sales
+WHERE Price > 55;
+SELECT * FROM SubLab66;
 
 # 7. Показать на экран среднее количество страниц по каждой из тематик, при этом показать только тематики, в которых среднее количество более 400.
 CREATE VIEW SubLab67 AS
-SELECT id_Theme, AVG(Pages) AS AvgPgs FROM 2f_books_info
-GROUP BY id_Theme;
+SELECT t.name, AVG(bi.Pages) AS AvgPgs FROM 2f_books_info AS bi
+JOIN 2f_themes AS t ON t.idthemes = bi.id_Theme
+GROUP BY bi.id_Theme;
 DROP VIEW SubLab67;
 SELECT * FROM SubLab67 WHERE AvgPgs > 560;
